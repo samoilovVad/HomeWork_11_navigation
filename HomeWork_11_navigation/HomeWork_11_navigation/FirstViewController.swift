0b8c433a9c640d98e3282e2ce33cf8079cd3ee03
Vadim <firstkoks@gmail.com>
2022-11-12 17:08:04 +0300

    add segues and unwind

--- HomeWork_11_navigation/HomeWork_11_navigation/FirstViewController.swift
+++ HomeWork_11_navigation/HomeWork_11_navigation/FirstViewController.swift
@@ -16,17 +16,38 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         updateData(with: updatedData)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateData(with: updatedData)
+    }
 
 
     @IBAction func editDataWithProperty(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         guard let second = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as? UpdatingDataController else { return }
-        second.updatingData = dataLabel.text ?? ""
+        second.updatingData = (sender as? UIButton)?.titleLabel?.text ?? ""
         navigationController?.pushViewController(second as! UIViewController, animated: true)
+    }
+    
+    @IBAction func unwindToFirstScreen(_ segue: UIStoryboardSegue) {
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "toEditScreen" : prepareEditScreen(segue)
+        default: break
+        }
     }
     
     private func updateData(with text: String) {
         dataLabel.text = text
+    }
+    
+    private func prepareEditScreen(_ segue: UIStoryboardSegue) {
+        guard let destination = segue.destination as? SecondViewController else {return}
+        destination.updatingData = dataLabel.text ?? ""
     }
 }
 
--- HomeWork_11_navigation/HomeWork_11_navigation/SecondViewController.swift
+++ HomeWork_11_navigation/HomeWork_11_navigation/SecondViewController.swift
@@ -29,4 +29,15 @@ class SecondViewController: UIViewController, UpdatingDataController {
         })
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case "toFirstScreen": prepareFirstScreen(segue)
+        default: break
+        }
+    }
+    
+    private func prepareFirstScreen(_ segue: UIStoryboardSegue) {
+        guard let destination = segue.destination as? FirstViewController else {return}
+        destination.updatedData = dataTextField.text ?? ""
+    }
 }
